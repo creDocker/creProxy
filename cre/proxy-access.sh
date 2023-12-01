e5882ca85a19f51c19c99caccef7bbcb947c3691
KMicha <michael.kahle@yahoo.de>
2023-12-01 17:22:20 +0100

    goaccess ends up using 100% cpu

--- cre/proxy-access.sh
+++ cre/proxy-access.sh
@@ -15,7 +15,9 @@ CRE_PROXY_ROOT="www/proxy"
 ## if [ /var/log/nginx/access.log -nt /cre/$CRE_PROXY_ROOT/access.html ] ; then
 if [ -f /var/log/nginx/access.log ]; then
   if [ $(echo " $CRE_VERSION < 2020.0" | bc) -eq 1 ] ; then
-    goaccess -f /var/log/nginx/access.log   --log-format="%h %^[%d:%^] \"%r\" %s %b \"%R\" \"%u\"" --date-format=%d/%b/%Y --time-format=%T --log-format=COMBINED > /cre/$CRE_PROXY_ROOT/access.html
+    ## goaccess -f /var/log/nginx/access.log   --log-format="%h %^[%d:%^] \"%r\" %s %b \"%R\" \"%u\"" --date-format=%d/%b/%Y --time-format=%T --log-format=COMBINED > /cre/$CRE_PROXY_ROOT/access.html
+    echo "goaccess not working as expected"
+    echo "<html><body><h1>goaccess not working</h1></body></html>" > /cre/$CRE_PROXY_ROOT/access.html
   else
     goaccess /var/log/nginx/access.log -o /cre/$CRE_PROXY_ROOT/access.html --anonymize-ip --log-format=COMBINED
   fi
